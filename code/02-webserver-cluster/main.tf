4502c57fc955131cb289c1b7d3a4d9912ea0e10b
FilipeTheAnalyst <fbalseiro@gmail.com>
2022-06-29 21:25:51 +0000

    Added code for cluster of web servers

---
+++ code/02-webserver-cluster/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+    # Required when using a launch configuration with an auto scaling group.
+    # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+    lifecycle {
+        create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
